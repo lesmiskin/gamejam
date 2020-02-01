d38367507571fcec09d257a849d24c6e425c569f
wizbanicus <jimihendrixhewasgood@gmail.com>
2020-02-01 13:37:56 +1300

    add theme player and sfx player scripts

---
+++ Assets/SFXPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    public string playSfx;
+    public bool play;
+    public float delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        playSfx = "ScytheCut";
+        play = false;
+        delay = .75f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play || Input.GetKeyDown(KeyCode.Space) )
+        {
+            playSound(playSfx, delay);
+            play = false;
+        }
+    }
+
+    void playSound(string sfx, float delay)
+    {
+        //float numF = Random.Range(1f, 4f);
+        int scytheAttackNumber = Mathf.FloorToInt(Random.Range(1f, 4f));
+        if (sfx != "")
+        {
+            audioSource.clip = Resources.Load<AudioClip>(sfx+scytheAttackNumber);
+            audioSource.PlayDelayed(delay);
+        }
+
+    }
+
+}
